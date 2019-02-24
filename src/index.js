e62e6988eaa6ccd1c7db7e7826ccdbee40b84cb5
Jose Valadez <vinmajj@gmail.com>
2019-02-23 18:01:52 -0800

    Incomplete up to Complete initializing the app with live data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // TODO: Render the `App` component to the DOM
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+//ReactDOM.render(<App livedata={searchYouTube}/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,4 +1,14 @@
-var searchYouTube = function (options, callback, errorCB = null) {
+var defaultOp = {
+  'key': window.YOUTUBE_API_KEY,
+  'max': 5,
+  'query': 'Blues'
+};
+
+var defaultCB = function (data) {
+  // Not sure
+};
+
+var searchYouTube = function (options = defaultOp, callback, errorCB = null) {
   // options will be based on user search input
   // TODO
   // declare the server
